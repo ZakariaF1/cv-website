6c43206ba3c66fba6edf30e0a9574872a39078a5
Zakaria Ahmad <zack_ah@hotmail.com>
2026-06-10 00:09:58 +0300

    contact form has colored icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,7 +24,7 @@ export default function Contact() {
             </svg>
           </a>
           <a href="https://linkedin.com/in/syzack" target="_blank" rel="noopener" className="social-link">
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+            <svg className="icon-linkedin" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
               <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 0 1-2.063-2.065 2.064 2.064 0 1 1 2.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
             </svg>
             linkedin.com/in/syzack
@@ -33,7 +33,7 @@ export default function Contact() {
             </svg>
           </a>
           <a href="tel:+40732911110" className="social-link">
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+            <svg className="icon-phone" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
               <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 13.1a19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 3.61 2h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 9.91a16 16 0 0 0 6.18 6.18l.95-.96a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z" />
             </svg>
             +40 732 911 110 · Romania
